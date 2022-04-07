07d6b88e43919cab2a0b16c5e801a4d0c94ba195
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-07 18:29:57 +0200

    metodo disegno rettangolo e implementazione su program completato.

---
+++ BONUS/Class1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BONUS
+{
+    internal class Class1
+    {
+
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+        private int area;
+        private int perimetro;
+
+        //----------- Costruttore ------------
+        public Class1(int baseRettangolo, int altezzaRettangolo)
+        {
+
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+
+        }
+
+        //----------- Metodo 1 perimetro rettangolo --------------
+
+        public int perimetroRettangolo()
+        {
+
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+            int perimetro = baseRettangolo + altezzaRettangolo + baseRettangolo + altezzaRettangolo;
+            this.perimetro = perimetro;
+            return this.perimetro;
+
+        }
+
+        //------------- Metodo 2 area rettangolo -----------------
+        public int areaRettangolo()
+        {
+
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+            int area = baseRettangolo * altezzaRettangolo;
+            this.area = area;
+            return this.area;
+
+        }
+
+        //-------------- Metodo 3 stampa dati rettangolo ------------
+        public void stampaRettangolo()
+        {
+            Console.WriteLine("------- RETTANGOLO -------");
+            Console.WriteLine("Base: " + baseRettangolo + " cm.");
+            Console.WriteLine("Altezza: " + altezzaRettangolo + " cm.");
+            Console.WriteLine("Perimetro: " + perimetro + " cm.");
+            Console.WriteLine("Area: " + area + " cm2");
+            Console.WriteLine("--------------------------");
+
+        }
+
+        //------------ Metodo 4 disegna rettangolo ------------------
+
+        public void disegnoRettangolo()
+        {
+            for (int i = 0; i < altezzaRettangolo; i++)
+            {
+                for (int j = 0; j < baseRettangolo; j++)
+                {
+                    
+                    if (i == 0 || i == (altezzaRettangolo - 1))
+                    {
+                        Console.Write("-");
+
+                    }
+                    
+                    else if (j == 0 || j == (baseRettangolo - 1))
+                    {
+                        Console.Write("|");
+                    }
+                    
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                
+                Console.Write("\n");
+            }
+        }
+    }
+}
+
+        
+    
+
+
---
+++ BONUS/Program.cs
@@ -0,0 +1,28 @@
+﻿using BONUS;
+
+//Inserisco i valori che passerò alla classe con readLine
+Console.WriteLine("Inserisci la base del rettangolo");
+int baseInput = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci altezza rettangolo");
+int altezzaInput = int.Parse(Console.ReadLine());
+
+//Creo l'oggetto
+Class1 mioRettangolo = new Class1(baseInput, altezzaInput);
+mioRettangolo.perimetroRettangolo();
+mioRettangolo.areaRettangolo();
+mioRettangolo.stampaRettangolo();
+mioRettangolo.disegnoRettangolo();
+
+//Inserisco i valori che passerò alla classe con readLine
+Console.WriteLine("Inserisci la base del rettangolo");
+int baseInput2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci altezza rettangolo");
+int altezzaInput2 = int.Parse(Console.ReadLine());
+
+//Creo un altro oggetto
+Class1 mioRettangolo2 = new Class1(baseInput2, altezzaInput2);
+
+mioRettangolo2.perimetroRettangolo();
+mioRettangolo2.areaRettangolo();
+mioRettangolo2.stampaRettangolo();
+mioRettangolo2.disegnoRettangolo();
