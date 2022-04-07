9cbcd9d72ba1cc98e3a3ce7082229eb1861bfee4
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-07 14:49:11 +0200

    class rettangolo creata e costruttore impostato

---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    //----------- Classe rettangolo --------------
+    public class Rettangolo
+    {
+
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+
+
+        //----------- Costruttore ------------
+        public Rettangolo(string baseRettangolo, string altezzaRettangolo)
+        {
+
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+
+        }
+    }
+
+}
