93bd98ecb5b9bfde6e56654ee541c60d407499a7
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-07 15:33:20 +0200

    calcolo di un secondo rettangolo

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -15,5 +15,18 @@ mioRettangolo.perimetroRettangolo();
 mioRettangolo.areaRettangolo();
 mioRettangolo.stampaRettangolo();
 
+//Inserisco i valori che passerò alla classe con readLine
+Console.WriteLine("Inserisci la base del rettangolo");
+int baseInput2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci altezza rettangolo");
+int altezzaInput2 = int.Parse(Console.ReadLine());
+
+//Creo un altro oggetto
+Rettangolo mioRettangolo2 = new Rettangolo(baseInput2 , altezzaInput2);
+
+mioRettangolo2.perimetroRettangolo();
+mioRettangolo2.areaRettangolo();
+mioRettangolo2.stampaRettangolo();
+
 
 
